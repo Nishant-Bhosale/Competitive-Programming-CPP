585599ae4d5edaed3315249e23ad5c4464b9f21e
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-11-18 22:16:12 +0530

    Added helpful maths problem

---
+++ CodeForces_Problems/Easy Problems/HelpFulMaths.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+   char mathsProb[101];
+   cin>>mathsProb;
+
+   cout<<(int)'9' - 48<<endl;
+   cout<<atoi(mathsProb);
+   return 0;
+}
