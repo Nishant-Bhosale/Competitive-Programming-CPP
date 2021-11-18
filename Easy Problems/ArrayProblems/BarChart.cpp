395a7d37d2dc6756eb6854d659b7ddea036019dc
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-11-18 09:11:49 +0530

    Added barchart problem

---
+++ Easy Problems/ArrayProblems/BarChart.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+   int inputNum, arrSize;
+
+   cin>>inputNum;
+   
+   vector<int> buildingHeight;
+   for (int i = 0; i < inputNum; i++){
+      cin>>arrSize;
+      buildingHeight.push_back(arrSize);
+   }
+
+   int maxHeight = buildingHeight[0];
+
+   for(int i = 0; i < buildingHeight.size(); i++){
+      if(maxHeight < buildingHeight[i]){
+         maxHeight = buildingHeight[i];
+      }
+   }
+
+   for(int floor = maxHeight; floor >= 1; floor--){
+      for(int i = 0; i <= buildingHeight.size(); i++){
+         if(buildingHeight[i] >= floor){
+            cout<<"*\t";
+         }else{
+            cout<<"\t";
+         }
+      }
+
+      cout<<endl;
+   }
+
+   return 0;
+}
