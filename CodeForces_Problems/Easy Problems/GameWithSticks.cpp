f10d36e8421444b1679c5590a1c328b33cadec9c
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-11-19 18:50:34 +0530

    Added gameWithSticks problem

---
+++ CodeForces_Problems/Easy Problems/GameWithSticks.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int row, col;
+
+   cin>>row>>col;
+
+   if(row > col){
+      swap(row, col);
+   }
+
+   if(row % 2 == 0){
+      cout<<"Malvika"<<endl;
+   }else{
+      cout<<"Akshat"<<endl;
+   }
+   return 0;
+}
