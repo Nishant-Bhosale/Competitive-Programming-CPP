02461b1b028da0a68a8f5c854a1bc69b54ce0ff9
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-11-23 13:51:22 +0530

    solved a problem again

--- Easy Problems/StackProblems/DuplicateBrackets.cpp
+++ Easy Problems/StackProblems/DuplicateBrackets.cpp
@@ -2,24 +2,51 @@
 #include<stack>
 using namespace std;
 
+// int main(){
+//    string str;
+//    stack<char> st;
+//     string ans = "false";
+//    getline(cin, str);
+
+//    for(int i = 0; i < str.size(); i++){
+//       char ch = str[i];
+//       if(ch == ')'){
+//          if(st.top() == '('){
+//             ans = "true";
+//          }else{
+//             while(st.top() != '('){
+//                 st.pop();
+//             }
+//             st.pop();
+//          }
+//       }else{
+//          st.push(ch);
+//       }
+//    }
+
+//    cout<<ans;
+//    return 0;
+// }
+
 int main(){
    string str;
    stack<char> st;
-    string ans = "false";
+   string ans = "false";
    getline(cin, str);
 
-   for(int i = 0; i < str.size(); i++){
+   for(int i = 0; i < str.length(); i++){
       char ch = str[i];
+
       if(ch == ')'){
          if(st.top() == '('){
             ans = "true";
          }else{
             while(st.top() != '('){
-                st.pop();
+               st.pop();
             }
             st.pop();
          }
-      }else{
+      }else {
          st.push(ch);
       }
    }
