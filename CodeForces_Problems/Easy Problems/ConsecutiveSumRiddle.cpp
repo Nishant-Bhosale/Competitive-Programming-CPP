bacd18a1836803e793c7b65f4fdf10f3663255b1
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-11-26 14:30:09 +0530

    Added problem

---
+++ CodeForces_Problems/Easy Problems/ConsecutiveSumRiddle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int t;
+   cin>>t;
+
+   while(t--){
+      long long n;
+      cin>>n;
+
+      cout<<-n + 1<<" "<<n<<endl;
+   }
+   return 0;
+}
