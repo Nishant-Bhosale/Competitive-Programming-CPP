06284c9272029eb21fce8ff979eb050f6013058e
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-11-27 14:00:39 +0530

    Moved files

---
+++ CodeForces_Problems/Medium Problems/DivanAndProject.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+   int t;
+
+   cin>>t;
+
+   while(t--){
+      int n, a;
+
+      cin>>n;
+
+      for(int i = 0; i < n; i++){
+         cin>>a;
+         
+      }
+   }
+
+   return 0;
+}
