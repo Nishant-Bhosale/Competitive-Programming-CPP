30abbeaaa888a549d6fa8c4078f7f2f656c6bb98
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-11-30 14:54:28 +0530

    Added keyboard problem

--- CodeForces_Problems/Medium Problems/Keyboard.cpp
+++ CodeForces_Problems/Medium Problems/Keyboard.cpp
@@ -1,28 +1,50 @@
+// #include<iostream>
+// #include <bits/stdc++.h>
+// #include<string>
+// using namespace std;
+
+// int main(){
+//    int t;
+
+//    cin>>t;
+
+//    for(int i = 0; i < 2*t; i++){
+//       string kb;
+//       string s;
+
+//       cin>>kb;
+//       cin>>s;
+
+//       int sum = 0;
+
+//       for(int j = 0; j < s.length() - 1; j++){
+         // sum += abs(int((kb.find(s[j]) - kb.find(s[j + 1]))));
+//       }
+
+//       cout<<sum<<endl;
+//    }
+
+//    return 0;
+// }
+
 #include<iostream>
-#include <bits/stdc++.h>
-#include<string>
 using namespace std;
 
 int main(){
    int t;
-
    cin>>t;
 
-   for(int i = 0; i < 2*t; i++){
-      string kb;
+   for(int i = 0; i < t; i++){
+      string keyb;
       string s;
 
-      cin>>kb;
-      cin>>s;
+      cin>>keyb>>s;
 
       int sum = 0;
-
-      for(int j = 0; j < s.length() - 1; j++){
-         sum += abs(int((kb.find(s[j]) - kb.find(s[j + 1]))));
+      for(int i = 0; i < s.length() - 1; i++){
+         sum += abs(int((keyb.find(s[i]) - keyb.find(s[i + 1]))));
       }
-
       cout<<sum<<endl;
    }
-
    return 0;
 }
