aefef7ea8a424f53a44254a88cb4bdc48d0dfa3d
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-12-15 20:55:53 +0530

    Added problems

--- d.cpp
+++ d.cpp
@@ -1,31 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+int t,n;
+bool pd;
+string s[100];
 int main(){
-   int t;
-   cin>>t;
-
-   while(t--){
-      int n;
-      cin>>n;
-
-      vector<string> arr;
-      for(int i = 0; i < n - 2; i++){
-         string st;
-         cin>>st;
-         arr.emplace_back(st);
-      }
-
-      string ans = "";
-      for(int i = 0; i < arr.size() - 1; i++){
-         if(arr[i][1] != arr[i + 1][0]){
-            ans += arr[i];
-         }else{
-            ans += (arr[i][0] + arr[i + 1][0]);
-         }
-      }
-
-      cout<<ans<<endl;
-   }
-   return 0;
+cin>>t;
+	while(t--){
+	cin>>n;pd=0;
+	for(int i=1;i<=n-2;++i){
+		cin>>s[i];
+	}
+	cout<<s[1];
+	for(int i=2;i<=n-2;++i){
+		if(s[i][0]==s[i-1][1])cout<<s[i][1];
+		else {
+			pd=1;
+			cout<<s[i];
+		}
+	}
+	if(pd==0)cout<<"a";
+	cout<<endl;
+	}
+	return 0;
 }
---
+++ e.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;
+   cin>>t;
+
+   while(t--){
+      int n;
+      cin>>n;
+
+      vector<int> arr;
+
+      for(int i = 0; i < n; i++){
+         int num;
+         cin>>num;
+         arr.emplace_back(num);
+      }
+
+      if(count(arr.begin(), arr.end(), arr[0]) == arr.size()){
+         cout<<0<<endl;
+      }else{
+         cout<<arr.size() - 1<<endl;
+      }
+
+
+   }
+   return 0;
+}
