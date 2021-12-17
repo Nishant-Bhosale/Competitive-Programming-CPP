8e7d04427fe608064830420a3bb23245da714558
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-12-17 21:59:59 +0530

    Added code

---
+++ b.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;
+   cin>>t;
+
+   while(t--){
+      int n;
+      cin>>n;
+
+      map<int, int> mp;
+
+      for(int i = 1; i <= n; i++){
+         int num;
+         cin>>num;
+
+         mp[num]++;
+      }
+
+      // for(auto it: mp){
+      //    cout<<it.second<<endl;
+      // }
+      cout<<(abs(mp[3] + mp[1]))<<endl;
+   }
+   return 0;
+}
