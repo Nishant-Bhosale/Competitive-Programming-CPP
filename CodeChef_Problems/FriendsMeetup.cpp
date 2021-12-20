3a43bf0f16d4a0f107bb8cd38fa865f26f220777
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-12-20 08:39:58 +0530

    Added codechef problem

---
+++ CodeChef_Problems/FriendsMeetup.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b) for(int i = a;i<b;i++)
+#define pi pair<int,int>
+
+typedef long long ll;
+
+int main(){
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+   int t;
+   cin>>t;
+   while(t--){
+      int a, b;
+      cin>>a>>b;
+
+      if(a >= b){
+         cout<<"YES"<<endl;
+      }else{
+         cout<<"NO"<<endl;
+      }
+   }
+   return 0;
+}
