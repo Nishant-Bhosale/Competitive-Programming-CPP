7f99f2564834475af50e748ba336fcfb44884ae0
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-01-07 12:19:15 +0530

    Added problem

---
+++ CodeForces_Problems/Medium Problems/BalancedTeam.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b) for(int i = a;i<b;i++)
+#define pi pair<int,int>
+
+typedef long long ll;
+
+int main(){
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+   ll n;
+   cin>>n;
+
+   vector<ll> arr;
+
+   for(int i = 0; i < n; i++){
+      ll num;
+      cin>>num;
+      arr.emplace_back(num);
+   }
+
+   sort(arr.begin(), arr.end());
+
+   ll maxVal = 0;
+   ll j = 0;
+   for(ll i = 0; i < n; i++){
+      while(j < n && arr[j] - arr[i] <= 5 ){
+         j++;
+         maxVal = max(maxVal, j - i);     
+      }
+   }
+
+   cout<<maxVal<<endl;
+
+   return 0;
+}
