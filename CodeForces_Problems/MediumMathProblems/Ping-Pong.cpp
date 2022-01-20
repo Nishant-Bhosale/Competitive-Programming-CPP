59d5b9d33c951144243065bc92eaf9ccdf350ee3
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-01-20 10:52:31 +0530

    Added problem

---
+++ CodeForces_Problems/MediumMathProblems/Ping-Pong.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b) for(int i = a;i<b;i++)
+#define pi pair<int,int>
+
+typedef long long ll;
+
+int main(){
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+   ll t;
+   cin>>t;
+
+   while(t--){
+      ll x, y;
+      cin>>x>>y;
+      cout<<(x-1)<<" "<<y<<endl;
+   }
+
+   return 0;
+}
