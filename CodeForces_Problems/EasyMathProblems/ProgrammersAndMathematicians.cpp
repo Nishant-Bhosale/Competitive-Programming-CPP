fe822db3b62d00756f18207387a4fd3aedb3b942
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-02-20 09:22:29 +0530

    added problem

---
+++ CodeForces_Problems/EasyMathProblems/ProgrammersAndMathematicians.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b) for(int i = a;i<b;i++)
+#define pi pair<int,int>
+
+typedef long long ll;
+
+int main(){
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+   ll t;
+   cin>>t;
+
+   while(t--){
+      ll a, b;
+      cin>>a>>b;
+
+      ll ans = min({a, b, (a + b) / 4});
+
+      cout<<ans<<endl;
+   }
+
+   return 0;
+}
