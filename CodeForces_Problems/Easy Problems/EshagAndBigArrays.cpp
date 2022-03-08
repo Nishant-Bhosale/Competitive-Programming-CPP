4feb7975eb75603ce12d13d0062b3219f9e14bb8
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-03-08 15:36:40 +0530

    Added problem

---
+++ CodeForces_Problems/Easy Problems/EshagAndBigArrays.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b) for(int i = a;i<b;i++)
+#define pi pair<int,int>
+
+typedef long long ll;
+
+int main(){
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+   ll t;
+   cin>>t;
+
+   while(t--){
+      ll n;
+      cin>>n;
+
+      vector<ll> arr(n); 
+      for(ll i = 0; i < n; i++){
+         cin>>arr[i];
+      }
+      
+      sort(arr.begin(), arr.end());
+
+      ll cnt = count(arr.begin(), arr.end(), arr[0]);
+
+      cout<<(n - cnt)<<endl;
+   }
+
+   return 0;
+}
