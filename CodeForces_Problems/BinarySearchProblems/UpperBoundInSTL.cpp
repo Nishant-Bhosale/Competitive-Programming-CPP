93230c1a9b10b9566db0a73495cee1486b270769
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-03-27 21:14:21 +0530

    Added problem

---
+++ CodeForces_Problems/BinarySearchProblems/UpperBoundInSTL.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b) for(int i = a;i<b;i++)
+#define pi pair<int,int>
+#define all(x) x.begin(), x.end()
+
+typedef long long ll;
+
+int main(){
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+   ll t;
+   cin>>t;
+   while(t--){
+      ll n, x;
+      cin>>n>>x;
+
+      vector<ll> arr(n);
+      for(ll i = 0; i < n; i++){
+         cin>>arr[i];
+      }
+
+      ll ind = upper_bound(all(arr), x) - arr.begin();
+      ind--;
+      if(ind >= 0 && arr[ind] == x){
+         cout<<ind<<endl;
+      }else{
+         cout<<-1<<endl;
+      }
+   };
+
+   return 0;
+}
