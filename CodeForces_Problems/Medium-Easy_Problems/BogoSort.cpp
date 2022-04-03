4bfe354387f3ee426d67ef05608f56d10abc9f4a
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-04-04 00:02:59 +0530

    Added problem

---
+++ CodeForces_Problems/Medium-Easy_Problems/BogoSort.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b) for(int i = a;i<b;i++)
+#define pi pair<int,int>
+#define all(x) x.begin(), x.end()
+
+typedef long long ll;
+
+int main(){
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+   ll t;
+   cin>>t;
+   while(t--){
+      ll n;
+      cin>>n;
+      vector<ll> arr(n);
+      for(ll i = 0;i<n;i++) cin>>arr[i];
+
+      sort(all(arr), greater<ll>());
+
+      for(ll i = 0; i < n; i++){
+         cout<<arr[i]<<" ";
+      }
+      cout<<"\n";
+   };
+
+   return 0;
+}
