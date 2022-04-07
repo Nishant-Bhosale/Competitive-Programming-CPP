29c633f0751fd9a089c1f2b84cc436ae889b42bd
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-04-07 12:42:37 +0530

    Added problem

---
+++ CodeForces_Problems/Medium-Easy_Problems/SifidAndStrangeSubsequence.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b) for(int i = a;i<b;i++)
+#define pi pair<int,int>
+#define all(x) x.begin(), x.end()
+
+typedef long long ll;
+
+int main(){
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+   ll t;
+   cin>>t;
+   while(t--){
+      ll n;
+      cin>>n;
+      vector<ll> arr(n);
+      ll ans = 0;
+      for(ll i = 0;i<n;i++) {
+         cin>>arr[i];
+         ans += (arr[i] <= 0);
+      }
+
+      sort(all(arr));
+
+      ll minVal = INT_MAX;
+
+      for(ll i = 0; i < n; i++){
+         if(arr[i] > 0){
+            minVal = min(minVal, arr[i]);
+         }
+      }
+
+      ll flag = minVal < INT_MAX;
+
+      for(ll i = 1; i < n; i++){
+         if(arr[i] <= 0){
+            flag &= (arr[i] - arr[i - 1] >= minVal);
+         }
+      }
+
+      if(flag){
+         cout<<ans + 1<<endl;
+      }else{
+         cout<<ans<<endl;
+      }
+   };
+
+
+
+   return 0;
+}
