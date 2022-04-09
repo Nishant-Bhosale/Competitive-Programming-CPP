cf57a3a6a2b3b74607d229147a68504daecc9aaf
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-04-09 23:29:14 +0530

    Added problem

---
+++ CodeForces_Problems/EasyMathProblems/GCDvsLCM.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b) for(int i = a;i<b;i++)
+#define pi pair<int,int>
+#define all(x) x.begin(), x.end()
+
+typedef long long ll;
+
+int main(){
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+   ll t;
+   cin>>t;
+   while(t--){
+      ll n;
+      cin>>n;
+
+      cout<<(n - 3)<<" "<<1<<" "<<1<<" "<<1<<"\n";
+   };
+
+   return 0;
+}
