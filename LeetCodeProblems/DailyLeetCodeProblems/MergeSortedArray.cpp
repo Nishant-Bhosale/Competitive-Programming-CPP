9e069f199e00db2e3af728cd5c6da28a03d0a45e
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-06-07 18:38:36 +0530

    Added optimized code

--- LeetCodeProblems/DailyLeetCodeProblems/MergeSortedArray.cpp
+++ LeetCodeProblems/DailyLeetCodeProblems/MergeSortedArray.cpp
@@ -19,4 +19,24 @@ public:
         
         sort(nums1.begin(), nums1.end());
     }
+
+   //  Optimized code
+   int last = m + n - 1;
+        
+        while(m > 0 && n > 0){
+            if(nums1[m - 1] > nums2[n - 1]){
+                nums1[last] = nums1[m - 1];
+                m--;
+            }else{
+                nums1[last] = nums2[n - 1];
+                n--;
+            }
+            last--;
+        }
+        
+        while(n > 0){
+            nums1[last] = nums2[n - 1];
+            n--;
+            last--;
+        }
 };
