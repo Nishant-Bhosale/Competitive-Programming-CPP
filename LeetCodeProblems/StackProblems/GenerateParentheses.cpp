bdceaf18d04e2fbfeaf49e004a100c7985d13c27
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-06-09 15:29:33 +0530

    Added problem

--- LeetCodeProblems/StackProblems/GenerateParentheses.cpp
+++ LeetCodeProblems/StackProblems/GenerateParentheses.cpp
@@ -1,29 +1,55 @@
+// class Solution {
+//     vector<string> ans;
+// public:
+//     vector<string> generateParenthesis(int n) {
+//         int len = n * 2;
+//         solve("", len);
+//         return ans;
+//     }
+// public:
+//     bool check(string s){
+//         int diff = 0;
+//         for(int i = 0; i < s.size(); i++){
+//             diff += (s[i] == '(' ? 1 : -1);
+//             if(diff < 0) return false;
+//         }
+//         return !diff;
+//     }
+    
+// public:
+//     void solve(string s, int n){
+//         if(s.size() == n){
+//             if(check(s)) ans.push_back(s);
+//             return;
+//         }
+        
+//         solve(s + '(', n);
+//         solve(s + ')', n);
+//     }
+// };
+
+//Optimized Solution
+
 class Solution {
     vector<string> ans;
 public:
     vector<string> generateParenthesis(int n) {
-        int len = n * 2;
-        solve("", len);
+        int len = n;
+        solve(0, 0, "", len);
         return ans;
     }
 public:
-    bool check(string s){
-        int diff = 0;
-        for(int i = 0; i < s.size(); i++){
-            diff += (s[i] == '(' ? 1 : -1);
-            if(diff < 0) return false;
-        }
-        return !diff;
-    }
-    
-public:
-    void solve(string s, int n){
-        if(s.size() == n){
-            if(check(s)) ans.push_back(s);
+    void solve(int open, int close, string s, int n){
+        if(s.size() == n * 2){
+            ans.push_back(move(s));
             return;
         }
         
-        solve(s + '(', n);
-        solve(s + ')', n);
+        if(open < n){
+            solve(open + 1, close, s + '(', n);        
+        }
+        if(close < open){
+            solve(open, close + 1, s + ')', n);        
+        }
     }
 };
