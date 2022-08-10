a6f4d9bcf6791cd0096084e8c63038ace7289174
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-08-10 19:03:58 +0530

    Added problem

---
+++ LeetCodeProblems/BinaryTreeProblems/BottomViewOfBinaryTree.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+        vector<int> ans;
+        map<int,int> mp;
+        queue<pair<Node*, int>> q;
+        
+        q.push({root, 0});
+        
+        while(!q.empty()){
+            auto pr = q.front();
+            q.pop();
+            Node* node = pr.first;
+            int dist = pr.second;
+            
+            mp[dist] = node->data;
+            if(node->left){
+                q.push({node->left, dist - 1});
+            }
+            if(node->right){
+                q.push({node->right, dist + 1});
+            }
+        }
+        
+        for(auto it : mp){
+            ans.push_back(it.second);
+        }
+        
+        return ans;
+    }
+};
