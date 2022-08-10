fab43fac1ca9238a60fd33e3e3f4c181ae21a1f3
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-08-10 15:28:55 +0530

    Added problem

---
+++ LeetCodeProblems/BinaryTreeProblems/VerticalOrderTraversalOfBinaryTree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int, map<int, multiset<int>>> nodes;
+        queue<pair<TreeNode*, pair<int,int>>> todo;
+        vector<vector<int>> ans;
+        
+        todo.push({root, {0, 0}});
+        
+        while(!todo.empty()){
+            auto p = todo.front();
+            todo.pop();
+            TreeNode* node = p.first;
+            int x = p.second.first, y = p.second.second;
+            nodes[x][y].insert(node->val);
+            if(node->left){
+                todo.push({node->left, {x - 1, y + 1}});
+            }
+            if(node->right){
+                todo.push({node->right, {x + 1, y + 1}});
+            }
+        }
+        
+        for(auto p : nodes){
+            vector<int> col;
+            for(auto q : p.second){
+                col.insert(col.end(), q.second.begin(), q.second.end());
+            }
+            ans.push_back(col);
+        }
+        
+        return ans;
+    }
+};
