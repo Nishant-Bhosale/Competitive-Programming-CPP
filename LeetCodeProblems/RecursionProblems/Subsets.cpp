7b105e2842410616465f89ea33bfff089de84551
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-08-24 17:11:29 +0530

    Added problem

---
+++ LeetCodeProblems/RecursionProblems/Subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    vector<vector<int>> ans;
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        getSubsets(0, temp, nums);
+        return ans;
+    }
+public: 
+    void getSubsets(int i, vector<int>& temp, vector<int>& nums){
+        if(i >= nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[i]);
+        getSubsets(i + 1, temp, nums);
+        // temp.erase(temp.begin() + i);
+        temp.pop_back();
+        getSubsets(i + 1, temp, nums);
+    }
+};
